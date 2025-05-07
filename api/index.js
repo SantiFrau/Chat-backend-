4a6bddd18d9a3f154603aefc492081ac339bf16a
Santi <santiagofrau3@gmail.com>
2025-05-07 18:55:11 -0300

    webSocket

--- api/index.js
+++ api/index.js
@@ -9,13 +9,19 @@ import createUserRoutes from './routes/userRoutes.js';
 import createChatsRoutes from './routes/chatRoutes.js';
 import chatModel from './model/chatModel.js';
 import userModel from './model/userModel.js';
+import { validateTokenSocket } from './middleware/validateToken.js';
 
 // Cargar variables de entorno
 dotenv.config();
 
+
+
 const app = express();
 const server = http.createServer(app); // Crear servidor HTTP con Express
 const PORT = process.env.PORT || 3000;
+
+
+
 
 // Middlewares
 app.use(cors({
@@ -46,10 +52,45 @@ const io = new Server(server, {
   }
 });
 
+io.use(validateTokenSocket)
 
+io.on('connection', (socket) => {
+  console.log('Nuevo cliente conectado:', socket.id);
+
+  // Escucha para unirse a una sala
+  socket.on('join', (chatsId) => {
+    chatsId.forEach(id => {
+      socket.join(id);
+      console.log(`Usuario ${socket.user.username} se unió a la sala ${id}`);
+    });
+  });
+
+  
+  socket.on('send-message', ({ chatId, message }) => {
+    // Obtener todos los sockets conectados a esa sala
+    const socketsInRoom = io.sockets.adapter.rooms.get(chatId);
+    
+    // Emitir el mensaje a todos los demás sockets en la sala (excepto al que lo envió)
+    if (socketsInRoom) {
+      socketsInRoom.forEach((socketId) => {
+        if (socketId !== socket.id) {
+          io.to(socketId).emit('receive-message', { message ,username:socket.user.username });
+          console.log(`${socket.user.username} envió un mensaje a ${socketId}`);
+        }
+      });
+    }
+
+    console.log(`${socket.user.username} envió un mensaje a la sala ${chatId}`);
+  });
+
+  // Evento de desconexión
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado:', socket.id);
+  });
+});
 
 
 // Iniciar servidor
 server.listen(PORT, () => {
-  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);   
 });
--- api/middleware/validateToken.js
+++ api/middleware/validateToken.js
@@ -24,3 +24,28 @@ export const validateToken = async (req, res, next) => {
     return res.status(401).json({ error: 'Token inválido o expirado', success: false });
   }
 };
+
+
+export const validateTokenSocket =(socket, next) => {
+  // Obtener el token del encabezado 'Authorization'
+  const token = socket.handshake.headers.authorization;
+
+  // Verificar si el token está presente
+  if (!token?.startsWith('Bearer ')) {
+    return next(new Error('Token no proporcionado'));
+  }
+
+  const actualToken = token.split(' ')[1]; // Extraer el token sin 'Bearer'
+
+  // Verificar el token usando JWT
+  jwt.verify(actualToken, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return next(new Error('Token inválido o expirado'));
+    }
+
+    // Agregar la información del usuario decodificada al socket para usarla en las emisiones posteriores
+    socket.user = decoded; 
+
+    next(); // Continuar con la conexión
+  });
+}
